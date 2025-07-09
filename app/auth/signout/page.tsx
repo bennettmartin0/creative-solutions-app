b6f5d1ad88998d852f41b19b174dd5b9f0b7277a
Bennett Martin <bennett.martin@csuglobal.edu>
2025-07-08 22:20:42 -0400

    deleted extra page causing error

--- app/auth/signout/page.tsx
+++
@@ -1,9 +0,0 @@
-import BlobTankNetwork from "@/app/components/BlobTankNetwork";
-
-export default function Page() {
-    return (
-        <div className="h-screen w-full bg-radial-gradient">
-            <BlobTankNetwork />
-        </div>
-    )
-}
